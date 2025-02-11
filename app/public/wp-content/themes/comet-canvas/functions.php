2e828848ae490a2c058e6341c06e2ffb1980908c
Leesa Ward <leesa@doubleedesign.com.au>
2025-02-11 22:15:06 +1100

    Theme colour support

--- app/public/wp-content/themes/comet-canvas/functions.php
+++ app/public/wp-content/themes/comet-canvas/functions.php
@@ -1,6 +1,7 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
-use Doubleedesign\CometCanvas\{SiteHealth, WpAdmin};
+use Doubleedesign\CometCanvas\{ThemeStyle, SiteHealth, WpAdmin};
 
+new ThemeStyle();
 new SiteHealth();
 new WpAdmin();
