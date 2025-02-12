fb9a7bbc3ee2dc07897abfb35271788d814c0740
Leesa Ward <leesa@doubleedesign.com.au>
2025-02-12 22:54:42 +1100

    Add menu support

--- app/public/wp-content/themes/comet-canvas/footer.php
+++ app/public/wp-content/themes/comet-canvas/footer.php
@@ -1,3 +1,10 @@
+<?php
+use Doubleedesign\CometCanvas\NavMenus;
+
+$menuItems = NavMenus::get_simplified_nav_menu_items_by_location('footer');
+// TODO: Footer component
+?>
+
 <?php wp_footer(); ?>
 </body>
 </html>
--- app/public/wp-content/themes/comet-canvas/functions.php
+++ app/public/wp-content/themes/comet-canvas/functions.php
@@ -1,7 +1,8 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
-use Doubleedesign\CometCanvas\{ThemeStyle, SiteHealth, WpAdmin};
+use Doubleedesign\CometCanvas\{ThemeStyle, NavMenus, SiteHealth, WpAdmin};
 
 new ThemeStyle();
+new NavMenus();
 new SiteHealth();
 new WpAdmin();
