6ef465d9ec6280b6f22ef5a6e610016970bd4f2d
Leesa Ward <leesa@doubleedesign.com.au>
2025-02-12 20:26:08 +1100

    Add default page header

--- app/public/wp-content/themes/comet-canvas/index.php
+++ app/public/wp-content/themes/comet-canvas/index.php
@@ -1,4 +1,18 @@
 <?php
+use Doubleedesign\Comet\Core\PageHeader;
 get_header();
+
+if(!is_front_page()) {
+	if(class_exists("Doubleedesign\Breadcrumbs\Breadcrumbs")) {
+		$breadcrumbs = Doubleedesign\Breadcrumbs\Breadcrumbs::$instance->get_raw_breadcrumbs();
+		$pageHeader = new PageHeader(['backgroundColor' => 'white', 'size' => 'narrow'], get_the_title(), $breadcrumbs);
+	}
+	else {
+		$pageHeader = new PageHeader(['backgroundColor' => 'white', 'size' => 'narrow'], get_the_title());
+	}
+
+	$pageHeader->render();
+}
+
 the_content();
 get_footer();
